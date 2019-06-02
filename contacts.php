43e8a14d007896132d8a98e86e155bd5d7c3c36e
DariaKozlov <darya.kozlovskaya.98@inbox.ru>
2019-06-03 00:14:40 +0400

    update contacts.php

--- contacts.php
+++ contacts.php
@@ -5,28 +5,30 @@
     <?php include "include/header.php"; ?>
     <?php include "include/menu.php"; ?>
 
-    <section id="blackboard">
+    <section id="blackboardcontacts">
     	<div class="container">
-    		<div class="alltextblackboard">
-    			<img src="/assets/images/karta.jpg" class="map">
-    		  <div class = "treatment1">
-            <img src="/assets/images/formula.png" class="formula">
-    		    <p class="contactsHeader">Контактные данные центра <span>Пифагор</span></p><br>
-        		<p>г.Самара,<br> ул.Ново-Садовая, д.106<br>
-              ТЦ "Захар" 5б этаж, офис 502 B.<br>8 (846) 990-70-53,<br>
-              8 927 2607053.<br> vk.com/pifagor63</p><br>
-    	    </div>
-          <div class="info">
-           	<div class="infobox">
-           	  <div class="tittleinfo">
-                <p>Запишись на <span>бесплатное</span> тестирование</p>
-              </div>
-           		<input type="text" class="nameinfobox" placeholder="Введи имя">
-           	  <input type="text" class="phoneinfobox" placeholder="Введи телефон">
-              <a href="#" class="zapicinfobox">Записаться на бесплатное тестирование</a>
-           	</div>
+        <div class="data">
+        <img src="/assets/images/karta.jpg" class="karta">
+        <div class="contdata">
+         <h3>Контактные данные</br>центра <span>пифагор</span></h3>
+
+          <p>г.Самара, </br>
+          ул.Ново-Садовая, д.106</br>
+          ТЦ «Захар» 5б этаж, офис 502 В</br>
+          8 (846) 990-70-53</br>
+          8 927 2607053</br>
+          vk.com/pifagor63</br></p>
+        </div>
+      </div>
+    		 <div class="info">
+          <div class="infobox">
+            <div class="tittleinfo"><p>Запишись на <span>бесплатное</span> тестирование</p></div>
+            <input type="text" class="nameinfobox" placeholder="Введи имя">
+            <input type="text" class="phoneinfobox" placeholder="Введи телефон">
+            <a href="#" class="zapicinfobox">Записаться на бесплатное тестирование</a>
           </div>
         </div>
+
       </div>
     </section>
 
