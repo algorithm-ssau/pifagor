5d5054dd8cfa77e73318ef97a55a904f8e270d42
Chipi_198 <rinat_batkaev@mail.ru>
2019-06-02 14:43:50 +0300

    refactoring

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
     		  <div class="textpifagor">Пифагор</div>
     		  <div class="lineinclineheader"></div>
     		  <div class="textcentrobr">Центр образования школьников</div>
-    		  <img src="/assets/icons/logophone.png" alt="Pifagor" class="logophone">
+    		  <img src="/assets/images/logophone.png" alt="Pifagor" class="logophone">
     		  <div class="textnumberphone">8 (846) 990 70 53</div>
     		  <a href="#" class="button">Заказать звонок</a>
     		</div>
